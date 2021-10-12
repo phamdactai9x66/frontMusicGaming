ade79a36bdc1bc5be4cab315c4d8b14921930ab4
Cau <cauindo113@gmail.com>
2021-10-12 16:27:35 +0700

    add heart and change playlist

--- src/page/client/pageClient/category/category.tsx
+++ src/page/client/pageClient/category/category.tsx
@@ -1,101 +1,171 @@
 import React, { useEffect } from 'react'
-import {Link} from "react-router-dom"
-
+import { Link } from "react-router-dom"
+import { AiOutlineDownload, AiOutlineLink } from 'react-icons/ai';
+import { BiHeart } from 'react-icons/bi';
+import { FiPlayCircle } from 'react-icons/fi';
+import { HiOutlineDotsCircleHorizontal } from 'react-icons/hi';
+import { Select, MenuItem } from "@mui/material";
 interface Category<T> {
 
 }
 
 const Category: React.FC<Category<any>> = ({ ...props }) => {
-    return (
-     <div className="container-category">
+  return (
+    <div className="container-category">
       <div className="banner-category">
         <img src="https://html.nkdev.info/goodgames/assets/images/gallery-7.jpg" alt="" />
       </div>
       <div className="box-category">
         <div className="box-title-category">
-        <h4 className="title_all">Tâm trạng và hoạt động</h4>
+          <h4 className="title_all">Tâm trạng và hoạt động</h4>
         </div>
         <div className="box-grid-category">
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
+          <div className="box">
+            <figure>
+              <img src="https://i.ytimg.com/vi/Yije8O6eGn8/maxresdefault.jpg" alt="" />
+            </figure>
+            <div className="icon-box">
+              <div>
+                <BiHeart className="icon" />
+                <FiPlayCircle className="icon" />
+                <HiOutlineDotsCircleHorizontal className="icon" />
+              </div>
+            </div>
+            <Select className="option">
+              <MenuItem>
+                <AiOutlineDownload /> Tải xuống
+                                    </MenuItem>
+              <MenuItem>
+                <AiOutlineLink /> Sao chép link
+                                    </MenuItem>
+            </Select>
+            <h6>Nhạc trẻ remix</h6>
           </div>
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
-          </div>
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
-          </div>
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
+          <div className="box">
+            <figure>
+              <img src="https://i.ytimg.com/vi/Yije8O6eGn8/maxresdefault.jpg" alt="" />
+            </figure>
+            <div className="icon-box">
+              <div>
+                <BiHeart className="icon" />
+                <FiPlayCircle className="icon" />
+                <HiOutlineDotsCircleHorizontal className="icon" />
+              </div>
+            </div>
+            <Select className="option">
+              <MenuItem>
+                <AiOutlineDownload /> Tải xuống
+                                    </MenuItem>
+              <MenuItem>
+                <AiOutlineLink /> Sao chép link
+                                    </MenuItem>
+            </Select>
+            <h6>Nhạc trẻ remix</h6>
           </div>
         </div>
       </div>
       <div className="box-category">
         <div className="box-title-category">
-        <h4 className="title_all">Quốc gia</h4>
+          <h4 className="title_all">Quốc gia</h4>
         </div>
         <div className="box-grid-category">
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
+        <div className="box">
+            <figure>
+              <img src="https://i.ytimg.com/vi/Yije8O6eGn8/maxresdefault.jpg" alt="" />
+            </figure>
+            <div className="icon-box">
+              <div>
+                <BiHeart className="icon" />
+                <FiPlayCircle className="icon" />
+                <HiOutlineDotsCircleHorizontal className="icon" />
+              </div>
+            </div>
+            <Select className="option">
+              <MenuItem>
+                <AiOutlineDownload /> Tải xuống
+                                    </MenuItem>
+              <MenuItem>
+                <AiOutlineLink /> Sao chép link
+                                    </MenuItem>
+            </Select>
+            <h6>Nhạc trẻ remix</h6>
           </div>
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
-          </div>
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
-          </div>
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
+          <div className="box">
+            <figure>
+              <img src="https://i.ytimg.com/vi/Yije8O6eGn8/maxresdefault.jpg" alt="" />
+            </figure>
+            <div className="icon-box">
+              <div>
+                <BiHeart className="icon" />
+                <FiPlayCircle className="icon" />
+                <HiOutlineDotsCircleHorizontal className="icon" />
+              </div>
+            </div>
+            <Select className="option">
+              <MenuItem>
+                <AiOutlineDownload /> Tải xuống
+                                    </MenuItem>
+              <MenuItem>
+                <AiOutlineLink /> Sao chép link
+                                    </MenuItem>
+            </Select>
+            <h6>Nhạc trẻ remix</h6>
           </div>
         </div>
       </div>
       <div className="box-category">
         <div className="box-title-category">
-        <h4 className="title_all">EDM</h4>
+          <h4 className="title_all">EDM</h4>
         </div>
         <div className="box-grid-category">
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
+        <div className="box">
+            <figure>
+              <img src="https://i.ytimg.com/vi/Yije8O6eGn8/maxresdefault.jpg" alt="" />
+            </figure>
+            <div className="icon-box">
+              <div>
+                <BiHeart className="icon" />
+                <FiPlayCircle className="icon" />
+                <HiOutlineDotsCircleHorizontal className="icon" />
+              </div>
+            </div>
+            <Select className="option">
+              <MenuItem>
+                <AiOutlineDownload /> Tải xuống
+                                    </MenuItem>
+              <MenuItem>
+                <AiOutlineLink /> Sao chép link
+                                    </MenuItem>
+            </Select>
+            <h6>Nhạc trẻ remix</h6>
           </div>
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
-          </div>
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
-          </div>
-          <div className="product-box-category">
-            <img src="https://thumbs.dreamstime.com/b/edm-club-music-party-template-dance-party-flyer-brochure-night-party-club-sound-banner-poster-72485529.jpg" alt="" />
-            <p>
-            EDM Club Music Party Template, Dance Party Flyer, Brochure. Night</p>
+          <div className="box">
+            <figure>
+              <img src="https://i.ytimg.com/vi/Yije8O6eGn8/maxresdefault.jpg" alt="" />
+            </figure>
+            <div className="icon-box">
+              <div>
+                <BiHeart className="icon" />
+                <FiPlayCircle className="icon" />
+                <HiOutlineDotsCircleHorizontal className="icon" />
+              </div>
+            </div>
+            <Select className="option">
+              <MenuItem>
+                <AiOutlineDownload /> Tải xuống
+                                    </MenuItem>
+              <MenuItem>
+                <AiOutlineLink /> Sao chép link
+                                    </MenuItem>
+            </Select>
+            <h6>Nhạc trẻ remix</h6>
           </div>
         </div>
       </div>
-     </div>
+    </div>
 
 
-    )
+  )
 }
 
 export default Category
