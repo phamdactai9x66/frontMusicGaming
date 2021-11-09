a3ab0e84d23af21be789bd56f4ad97100d935a78
hoàng anh tú <hoanganhtu6969@gmail.com>
2021-11-09 09:36:57 +0700

    them remove modal

--- src/page/client/pageClient/home/home.tsx
+++ src/page/client/pageClient/home/home.tsx
@@ -98,7 +98,8 @@ const Home: React.FC<Home<any>> = ({ ...props }) => {
 
     return (
         <>
-   
+      
+        {/* // */}
           <div className="home">
             <div className="slider-banner">
                 <VerticalSlider settings_banner={settings_banner} />
