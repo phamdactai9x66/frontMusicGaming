d6c8ac52229a9f3dd8efce98f8752b1e4b35337b
taipham231 <tai15122003311@gmail.com>
2021-11-20 20:54:40 +0700

    add role according to user.

--- src/api/configApi.tsx
+++ src/api/configApi.tsx
@@ -3,19 +3,21 @@ import dataStoreage from "component/dataStorage";
 
 const baseURL = 'http://localhost:5000'
 
+const getUser = JSON.parse(localStorage?.getItem("persist:root") as any).user;
+const getToken = getUser ? JSON.parse(getUser).token : '';
 
 const Axios = axios.create({
     baseURL,
     headers: {
         'Content-Type': 'application/json',
-        Authorization: dataStoreage.accessToken || ''
+        Authorization: dataStoreage.accessToken || getToken
     }
 })
 const AxiosFormdata = axios.create({
     baseURL,
     headers: {
         'Content-Type': 'application/form-data',
-        Authorization: dataStoreage.accessToken || ''
+        Authorization: dataStoreage.accessToken || getToken
     }
 })
 Axios.interceptors.response.use((response: AxiosResponse) => {
--- src/page/admin/rootPageAdmin.tsx
+++ src/page/admin/rootPageAdmin.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./admin.scss"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "../partials/admin/header";
 import Footer from "../partials/admin/footer";
 import Sidebar from "../partials/admin/sidebar";
+
+import { useSelector } from "react-redux";
+import { formStateUser } from 'redux/user/stateUser';
+import dataStorage from "component/dataStorage"
 interface RootPageAdmin<T> {
 
 }
 
 const RootPageAdmin: React.FC<RootPageAdmin<any>> = ({ ...props }) => {
+    const state = useSelector<{ user: any }>(state => state.user) as formStateUser;
+
+    useEffect(() => {
+        state.token && (dataStorage.accessToken = state.token)
+    }, [state])
     return (
         <>
             <div className="container-admin">
