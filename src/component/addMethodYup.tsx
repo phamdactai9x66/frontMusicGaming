8766522bffc7082b40bb19bc40b95cae3a85f84f
Tungnh89 <nguyenhuytung23032001@gmail.com>
2021-12-08 01:37:14 +0700

    fix styles table user and add field user

--- src/component/addMethodYup.tsx
+++ src/component/addMethodYup.tsx
@@ -13,6 +13,14 @@ Yup.addMethod(Yup.string, "checkRequire", function (message: string = messageDef
         return true;
     })
 })
+Yup.addMethod(Yup.number, "checkRequire", function (message: string = messageDefault) {
+    return this.test("checkRequire", message, function (value, field) {
+        const { path, createError } = this
+        if ([undefined, null, 0].includes(value)) return createError({ path, message })
+
+        return true;
+    })
+})
 Yup.addMethod(Yup.mixed, "requireFile", function (message: string = messageDefault) {
     return this.test("checkFile", message, function (value, field) {
         if (typeof value === "string") value = value.trim();
