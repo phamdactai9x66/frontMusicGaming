6f895de4171310940ec974559e7b3e74be80749c
taipham231 <tai15122003311@gmail.com>
2021-12-18 00:47:37 +0700

    remove button register in modal require login.

--- src/page/notificationModal/NotificationModal.tsx
+++ src/page/notificationModal/NotificationModal.tsx
@@ -12,14 +12,14 @@ const Notification: React.FC<notification<any>> = ({ ...props }) => {
     const wrapperRef = useRef(null);
     const location = useLocation();
 
-    const  useOutsideAlerter = (ref: any) => {
-        useEffect(() => { 
+    const useOutsideAlerter = (ref: any) => {
+        useEffect(() => {
             function handleClickOutside(event: any) {
                 if (ref.current && !ref.current.contains(event.target)) {
                     props.handleLogged();
                 }
             }
-    
+
             document.addEventListener("mousedown", handleClickOutside);
             return () => {
                 document.removeEventListener("mousedown", handleClickOutside);
@@ -29,14 +29,14 @@ const Notification: React.FC<notification<any>> = ({ ...props }) => {
 
     useOutsideAlerter(wrapperRef);
     return (
-        <div className="w-100 h-100 d-flex position-fixed top-0 text-center" style={{left:"0px",zIndex:10,backgroundColor:"rgb(0 0 0 / 25%)"}}>
-            <div ref={wrapperRef} className="my-auto mx-auto p-4 rounded-3" style={{backgroundColor:"#9cf6ff"}}>
+        <div className="w-100 h-100 d-flex position-fixed top-0 text-center" style={{ left: "0px", zIndex: 10, backgroundColor: "rgb(0 0 0 / 25%)" }}>
+            <div ref={wrapperRef} className="my-auto mx-auto p-4 rounded-3" style={{ backgroundColor: "#9cf6ff" }}>
                 <img className="w-25 h-25" src={avatar} alt="" />
 
-                <p style={{fontWeight:500}} className="mb-0">Hãy đăng nhập để có thể sử dụng tính năng này</p>
+                <p style={{ fontWeight: 500 }} className="mb-0">Hãy đăng nhập để có thể sử dụng tính năng này</p>
 
                 <p>Tính năng này chỉ dành cho người dùng đã có tài khoản Music Game</p>
-                
+
                 <div className="d-flex justify-content-center">
                     <Link to={{
                         pathname: "/signin",
@@ -50,8 +50,8 @@ const Notification: React.FC<notification<any>> = ({ ...props }) => {
                             lastLocation: props.path ? props.path : location,
                         },
                     }}
-                        className="" style={{marginRight:"0.2rem"}}>
-                        <button onClick={()=>props.handleLogged()} type="button" className="btn btn-light">Đăng kí</button>
+                        className="" style={{ marginRight: "0.2rem" }}>
+                        {/* <button onClick={()=>props.handleLogged()} type="button" className="btn btn-light">Đăng kí</button> */}
                     </Link>
                     <Link to={{
                         pathname: '/signin',
@@ -59,7 +59,7 @@ const Notification: React.FC<notification<any>> = ({ ...props }) => {
                             lastLocation: props.path ? props.path : location,
                         },
                     }}>
-                        <button onClick={()=>props.handleLogged()} type="button" className="btn btn-primary" style={{marginLeft:"0.2rem"}}>Đăng nhập</button>
+                        <button onClick={() => props.handleLogged()} type="button" className="btn btn-primary" style={{ marginLeft: "0.2rem" }}>Đăng nhập</button>
                     </Link>
                 </div>
             </div>
